5d8a5d6389463f2ccdbc236cc06eb5d46fa9364e
vraj90523 <45808699+vraj90523@users.noreply.github.com>
2019-02-17 16:12:18 +0530

    Update global_bans.py

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -157,6 +157,8 @@ def ungban(bot: Bot, update: Update, args: List[str]):
 
     banner = update.effective_user  # type: Optional[User]
 
+        message.reply_text("I'll give {} a second chance, globally.".format(user_chat.first_name))
+
 
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
                  "<b>Regression of Global Ban</b>" \
